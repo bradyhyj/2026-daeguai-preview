c2a2f32d30640be6b044cfc41bbb3cb54345290d
rlarhdms1128 <143855484+rlarhdms1128@users.noreply.github.com>
2026-03-20 01:27:00 +0900

    Add History page route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import CautionReport from './pages/CautionReport.jsx';
 import ContractReport from './pages/ContractReport.jsx';
 import Chat from './pages/Chat.jsx';
 import MyPage from './pages/MyPage.jsx';
-// 👇 8. 북마크 페이지를 불러옵니다.
 import Bookmark from './pages/Bookmark.jsx';
+import History from './pages/History.jsx';
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
           {/* 2. 서류 업로드 및 분석 진행 화면 */}
           <Route path="/search" element={<Search />} />
 
-          {/* 3. 북마크(저장 목록) 화면 👈 추가됨! */}
+          {/* 3. 북마크(저장 목록) 화면 */}
           <Route path="/bookmark" element={<Bookmark />} />
           
           {/* 4. 일정 관리 화면 */}
@@ -39,6 +39,9 @@ function App() {
 
           {/* 8. 마이페이지 (프로필 및 기록) */}
           <Route path="/mypage" element={<MyPage />} />
+
+          {/* 9. 히스토리 화면 */}
+          <Route path="/history" element={<History />} />
         </Routes>
       </Layout>
     </Router>
